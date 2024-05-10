02dbcbccdd0f4cd7a5637b1605d2c4b4320d99dc
Ji Kaiwen <kevin0x00@163.com>
2024-05-10 09:07:00 +0800

    add k_assert

--- include/kutils/utils.h
+++ include/kutils/utils.h
@@ -1,8 +1,12 @@
 #ifndef _K_INCLUDE_KUTILS_UTILS_H_
 #define _K_INCLUDE_KUTILS_UTILS_H_
 
+#include <assert.h>
+
 #define k_likely(expr)    (__builtin_expect(!!(expr), 1))
 #define k_unlikely(expr)  (__builtin_expect(!!(expr), 0))
+
+#define k_assert(expr)    assert(expr)
 
 
 #endif
